e31bb8a39fb734c28439d5612c2e5e7149b4283b
MartheUwir <marthuwir@gmail.com>
2023-02-22 21:04:03 +0200

    print main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
+#include <unistd.h>
 #include "main.h"
 
+
 /**
- * main - Entry point
- *
-*/
+ * description
+ * this program prints main word followed by the new line
+ * Return: 0
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
