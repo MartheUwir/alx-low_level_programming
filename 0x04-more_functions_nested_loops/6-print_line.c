8c24b31e8e00aaa4e6f209fd5b2898d109a2131b
MartheUwir <marthuwir@gmail.com>
2023-02-25 04:42:26 +0200

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - is  function that draws nrew line in terminal.
+ @n: An input integer
+ * new line follows this.
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
