77ee9fcbb4b47c25f6f85214905ec7d0267cc483
MartheUwir <marthuwir@gmail.com>
2023-02-27 08:35:46 +0200

    fifth-about-arrays and pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,12 +7,18 @@
  *
  * Return: void
  */
+ void reset_to_98(int *n)
+{
+    *n = 98;
+
 {
     int n;
 
     n = 402;
     printf("n=%d\n", n);
     reset_to_98(&n);
-    printf("n=%d\n", n);
+   
+}
+ printf("n=%d\n", n);
     return (0);
 }
