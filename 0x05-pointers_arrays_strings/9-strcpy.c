3d2b1df654cc8fece87a235b41b8423b3851a1d6
MartheUwir <marthuwir@gmail.com>
2023-02-27 21:13:18 +0200

    pointers and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src 
+ *_strcpy - copies the string pointed to by src 
  *to the buffer pointed to by dest
  * @dest: buffer to copy the string to
  * @src: string to be copied
