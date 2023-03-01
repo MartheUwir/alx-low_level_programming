322cc91786dc0281857cdc29ae372154eaed92cc
MartheUwir <marthuwir@gmail.com>
2023-03-01 22:11:11 +0200

    sixth

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to capitalize.
+ *
+ * Return: A pointer to the capitalized string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	/* Capitalize first character of string */
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+
+	/* Capitalize words after separators */
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			i++;
+
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+	}
+
+	return (str);
+}
