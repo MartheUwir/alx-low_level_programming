2c9d07b952395857f72c7f79d406067beefd4e2a
MartheUwir <marthuwir@gmail.com>
2023-03-01 21:15:21 +0200

    sixth

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings.
+ *
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Maximum number of bytes to be copied from the source string.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0, i;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+arrays and pointers second project. THis project contains many programs entricing 
+functions and arrays. we will dive deepe in arrays and pointers .more understanding within this program
