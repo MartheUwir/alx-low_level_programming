6bfdcc983882a261b87dfcbd798aed3583cd50e1
MartheUwir <marthuwir@gmail.com>
2023-03-01 21:33:15 +0200

    sixth

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,8 +2,8 @@
 
 /**
  * _strncpy - copies a string
- * @dest: pointer to destination string
- * @src: pointer to source string
+ * @dest: destination string
+ * @src: source string
  * @n: maximum number of characters to copy
  *
  * Return: pointer to destination string
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return dest;
