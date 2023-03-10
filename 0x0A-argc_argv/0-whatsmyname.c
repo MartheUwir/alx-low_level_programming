ebba645f5ecc7465a3d56009d6cf46b0fefef662
MartheUwir <marthuwir@gmail.com>
2023-03-10 14:44:24 +0200

    All-in-all

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,32 +1,13 @@
-#include <stdio.h>
-		
-#include "main.h"
-		
-
-		
-/**
-		
- * main - prints the name of the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
- */
-		
-int main(int argc __attribute__((unused)), char *argv[])
-		
-{
-		
-printf("%s\n", *argv);
-		
-
-		
-return (0);
-		
+#include <unistd.h>                                                                                                                  
+                                                                                                                                     
+/**                                                                                                                                  
+ * _putchar - writes the character c to stdout                                                                                       
+ * @c: The character to print                                                                                                        
+ *                                                                                                                                   
+ * Return: On success 1.                                                                                                             
+ * On error, -1 is returned, and errno is set appropriately.                                                                         
+ */                                                                                                                                  
+int _putchar(char c)                                                                                                                 
+{                                                                                                                                    
+return (write(1, &c, 1));                                                                                                            
 }
-
