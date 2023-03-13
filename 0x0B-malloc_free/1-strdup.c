306cb75646e369f5cc13061255bdb0dda702dfa8
MartheUwir <marthuwir@gmail.com>
2023-03-13 14:56:41 +0200

    memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,14 +4,10 @@
 		
 #include "main.h"
 		
-/**
-		
- * _strdup - duplicate to new memory space location
-		
- * @str: char
-		
- * Return: 0
-		
+/**		
+ * _strdup - duplicate to new memory space location	
+ * @str: char		
+ * Return: 0	
  */
 		
 char *_strdup(char *str)
@@ -21,15 +17,9 @@ char *_strdup(char *str)
 char *aaa;
 		
 int i, r = 0;
-		
-
-		
 if (str == NULL)
-		
 return (NULL);
-		
 i = 0;
-		
 while (str[i] != '\0')
 		
 i++;
