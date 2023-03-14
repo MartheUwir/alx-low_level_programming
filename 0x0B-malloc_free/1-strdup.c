78a8c3a7b070a675828dcb5b5a29964c10c9911f
MartheUwir <marthuwir@gmail.com>
2023-03-14 13:48:48 +0200

    memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,48 @@
-#include <stdio.h>	
-#include <stdlib.h>		
-#include "main.h"		
-/**		
- * _strdup -duplicate to new memory space location	
- * @str:char		
- * Return:0	
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ *space in memory, which contains a copy of the
+ *string given as a parameter.
+ *@str:String to be copied
+ *
+ * Return: NULL in case of error, pointer to allocated
+ * space
+ *
+ * FUNCTIONALITY
+ *
+ * 1. First, we check if the string is NULL.  If it is, we return NULL.
+ * 2.  Next, we loop through the string to find the length of the string.
+ * 3.  We allocate memory for the copy of the string.
+ * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
+ * 5.  We loop through the string again, this time copying the string into the new memory.
+ * 6.  We add a null terminator to the end of the string.
+ * 7.  We return the copy of the string.
+ *
+ * Time Complexity: O(n)
  */
-		
-char *_strdup(char *str)		
-{		
-char *aaa;		
-int i, r=0;
-if (str == NULL)
-return (NULL);
-i = 0;
-while (str[i] != '\0')		
-i++;		
-aaa = malloc(sizeof(char) * (i + 1));		
-if (aaa == NULL)		
-return (NULL);		
-for (r = 0; str[r]; r++)		
-aaa[r] = str[r];
-return (aaa);
-		
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
 }
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,16 @@
+Write a function that creates an array of characters with a specific character and returns a pointer to the array. Returns NULL if size is 0.
+
+Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter. Returns NULL if str is NULL. On success, the function returns a pointer to the duplicated string.
+
+Write a function that concatenates two strings and returns a pointer to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated. If NULL is passed, treat it as an empty string. The function should return NULL on failure.
+
+Write a function that returns a pointer to a 2 dimensional array of integers. Each element of the grid should be initialized to 0. The function should return NULL on failure. If width or height is 0 or negative, return NULL.
+
+Write a function that frees a 2 dimensional grid previously created by the alloc_grid function.
+
+Write a function that concatenates all the arguments of the program and returns a pointer to a new string. Returns NULL if ac is 0 or av is NULL. Each argument should be followed by a newline character in the new string.
+
+Write a function that splits a string into words and returns a pointer to an array of strings (words). The last element of the returned array should be NULL. Words are separated by spaces. Returns NULL if str is NULL or an empty string. If the function fails, it should return NULL.
+
+
+
