c0e3e5e18a636e6baadc82df59e01c3b76023260
MartheUwir <marthuwir@gmail.com>
2023-03-15 08:41:23 +0200

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,84 +2,59 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 /**
-* check_num - check - string there are digit
-* @str: array str
-*
-* Return: Always 0 (Success)
-*/
+ * check_num - check if a string contains only digits
+ * @str: the string to check
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise
+ */
 int check_num(char *str)
 {
-/*Declaring variables*/
-unsigned int count;
+	/* declare variables */
+	unsigned int count;
 
-
-count = 0; 
-while (count < strlen(str)) /*count string*/
-
-
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count])) /* check if str contains only digits */
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
 }
-
-
-count++;
-}
-return (1);
-}
-
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - entry point of the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of pointers to the arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 on error
  */
-
-
 int main(int argc, char *argv[])
-
-
 {
+	/* declare variables */
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-
-		/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
-
-
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if (check_num(argv[count]))
-
-
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-sum += str_to_int;
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /* ATOI: convert string to int */
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-
-
-/*Condition if one of the number contains symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
-
-
-count++;
-}
-
-
-printf("%d\n", sum); /*print sum*/
-
-
-return (0);
-}
-
-
