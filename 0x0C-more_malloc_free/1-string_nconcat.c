ffcb5a17acc369f338affeb156ba6da557eab4db
MartheUwir <marthuwir@gmail.com>
2023-03-16 15:04:15 +0200

    All

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,36 +12,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int i = 0, j = 0, s1_len = 0, s2_len = 0;
+char *result;
+unsigned int i = 0, j = 0, s1_len = 0, s2_len = 0;
 
-    while (s1 && s1[s1_len])
-        s1_len++;
+while (s1 && s1[s1_len])
+s1_len++;
 
-    while (s2 && s2[s2_len])
-        s2_len++;
+while (s2 && s2[s2_len])
+s2_len++;
 
-    if (n < s2_len)
-        result = malloc(sizeof(char) * (s1_len + n + 1));
-    else
-        result = malloc(sizeof(char) * (s1_len + s2_len + 1));
+if (n < s2_len)
+result = malloc(sizeof(char) * (s1_len + n + 1));
+else
+result = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
-    if (!result)
-        return (NULL);
+if (!result)
+return (NULL);
 
-    while (i < s1_len)
-    {
-        result[i] = s1[i];
-        i++;
-    }
+while (i < s1_len)
+{
+result[i] = s1[i];
+i++;
+}
 
-    while (n < s2_len && i < (s1_len + n))
-        result[i++] = s2[j++];
+while (n < s2_len && i < (s1_len + n))
+result[i++] = s2[j++];
 
-    while (n >= s2_len && i < (s1_len + s2_len))
-        result[i++] = s2[j++];
+while (n >= s2_len && i < (s1_len + s2_len))
+result[i++] = s2[j++];
 
-    result[i] = '\0';
+result[i] = '\0';
 
-    return (result);
+return (result);
+
 }
