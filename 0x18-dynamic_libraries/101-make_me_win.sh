9074016ffb0bbc8af1d78d2b588578cfd02f94bf
MartheUwir <marthuwir@gmail.com>
2023-06-19 21:03:22 +0200

    well good

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-#!/bin/
-export LD_PRELOAD="$PWD/../libgiga.so"
+#!/bin/bash
+wget -P /tmp https://github.com/MartheUwir/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
